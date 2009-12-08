43fcbcc808d9d188f064d616d193385b00fbe33f
Paul MERLIN <paul@nosphere.org>
2009-12-08 12:38:07 +0100

    code cleanup

--- src/main/java/org/freedesktop/tango/App.java
+++ src/main/java/org/freedesktop/tango/App.java
@@ -4,10 +4,12 @@ package org.freedesktop.tango;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
+
 }
